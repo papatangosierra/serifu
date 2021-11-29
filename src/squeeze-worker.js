315087f7831b51a13a80915588000ef6237cd11b
papatangosierra <pts@sockdolager.net>
2021-11-29 22:14:37 +0000

    squeeze worker added

---
+++ src/squeeze-worker.js
@@ -0,0 +1,18 @@
+/* squeeze-worker.js is a web worker that takes the document text as its input, 
+runs the squeeze compression on it, and returns the compressed data structure
+to the main script */
+
+import { Squeezer } from "./squeeze.js";
+
+// actual web worker code starts here
+onmessage = (e) => {
+  console.log(`worker got message from main script: ${e.data}`);
+  if (typeof e.data === "string") {
+    let s = new Squeezer(e.data.replace(/\u001f/g, "")).squozed; // strip out \u001f if it's there (it probably isn't, but if it is, for some reason, we'll lose data, because it's the separator for Squeezer dictionary fields.
+    postMessage([s.uniques, s.seq]); // send uniques and sequence back to main thread
+  } else {
+    console.err("squeeze worker not given string to compress; giving up.");
+    postMessage(null);
+  }
+  console.log("compression done, returning result");
+};
